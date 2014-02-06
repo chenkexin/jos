3ac9a7f3df3e4438e4e0f97c1441f877f0f8953e
chenkexin <chenkexin.cq@gmail.com>
2014-02-06 14:55:45 +0800

    pass ex5

--- kern/trap.c
+++ kern/trap.c
@@ -65,36 +65,36 @@ trap_init(void)
 	extern struct Segdesc gdt[];
 
 	// LAB 3: Your code here.
- void th0();
-    void th1();
-    void th3();
-    void th4();
-    void th5();
-    void th6();
-    void th7();
-    void th8();
-    void th9();
-    void th10();
-    void th11();
-    void th12();
-    void th13();
-    void th14();
-    void th16();
-    SETGATE(idt[0], 0, GD_KT, th0, 0);
-    SETGATE(idt[1], 0, GD_KT, th1, 0);
-    SETGATE(idt[3], 0, GD_KT, th3, 0);
-    SETGATE(idt[4], 0, GD_KT, th4, 0);
-    SETGATE(idt[5], 0, GD_KT, th5, 0);
-    SETGATE(idt[6], 0, GD_KT, th6, 0);
-    SETGATE(idt[7], 0, GD_KT, th7, 0);
-    SETGATE(idt[8], 0, GD_KT, th8, 0);
-    SETGATE(idt[9], 0, GD_KT, th9, 0);
-    SETGATE(idt[10], 0, GD_KT, th10, 0);
-    SETGATE(idt[11], 0, GD_KT, th11, 0);
-    SETGATE(idt[12], 0, GD_KT, th12, 0);
-    SETGATE(idt[13], 0, GD_KT, th13, 0);
-    SETGATE(idt[14], 0, GD_KT, th14, 0);
-    SETGATE(idt[16], 0, GD_KT, th16, 0);
+  void th0();
+  void th1();
+  void th3();
+  void th4();
+  void th5();
+  void th6();
+  void th7();
+  void th8();
+  void th9();
+  void th10();
+  void th11();
+  void th12();
+  void th13();
+  void th14();
+  void th16();
+  SETGATE(idt[0], 0, GD_KT, th0, 0);
+  SETGATE(idt[1], 0, GD_KT, th1, 0);
+  SETGATE(idt[3], 0, GD_KT, th3, 0);
+  SETGATE(idt[4], 0, GD_KT, th4, 0);
+  SETGATE(idt[5], 0, GD_KT, th5, 0);
+  SETGATE(idt[6], 0, GD_KT, th6, 0);
+  SETGATE(idt[7], 0, GD_KT, th7, 0);
+  SETGATE(idt[8], 0, GD_KT, th8, 0);
+  SETGATE(idt[9], 0, GD_KT, th9, 0);
+  SETGATE(idt[10], 0, GD_KT, th10, 0);
+  SETGATE(idt[11], 0, GD_KT, th11, 0);
+  SETGATE(idt[12], 0, GD_KT, th12, 0);
+  SETGATE(idt[13], 0, GD_KT, th13, 0);
+  SETGATE(idt[14], 0, GD_KT, th14, 0);
+  SETGATE(idt[16], 0, GD_KT, th16, 0);
 	// Per-CPU setup 
 	trap_init_percpu();
 }
@@ -173,6 +173,15 @@ trap_dispatch(struct Trapframe *tf)
 	// Handle processor exceptions.
 	// LAB 3: Your code here.
 
+	if(tf->tf_trapno >= 0 && tf->tf_trapno < 32)
+	{
+		switch(tf->tf_trapno)
+		{
+			case T_PGFLT://page fault
+				page_fault_handler(tf);
+				break;
+		}
+	}
 	// Unexpected trap: The user process or the kernel has a bug.
 	print_trapframe(tf);
 	if (tf->tf_cs == GD_KT)
