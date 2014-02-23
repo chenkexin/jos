b16b57f38191692651714619c9af3b5db42b051d
chenkexin <chenkexin.cq@gmail.com>
2014-02-24 04:39:45 +0800

    lab4, ex12

--- lib/fork.c
+++ lib/fork.c
@@ -35,7 +35,24 @@ pgfault(struct UTrapframe *utf)
 
 	// LAB 4: Your code here.
 
-	panic("pgfault not implemented");
+  if (!((err & FEC_WR) && (uvpd[PDX(addr)] & PTE_P) && (uvpt[PGNUM(addr)] & PTE_P) && (uvpt[PGNUM(addr)] & PTE_COW)))
+      panic("not copy-on-write");
+
+  addr = ROUNDDOWN(addr, PGSIZE);
+  
+  if (sys_page_alloc(0, PFTEMP, PTE_W|PTE_U|PTE_P) < 0)
+      panic("sys_page_alloc");
+   
+  memcpy(PFTEMP, addr, PGSIZE);
+ 
+  if (sys_page_map(0, PFTEMP, 0, addr, PTE_W|PTE_U|PTE_P) < 0)
+      panic("sys_page_map");
+
+  if (sys_page_unmap(0, PFTEMP) < 0)
+      panic("sys_page_unmap");
+ 
+  return;
+//	panic("pgfault not implemented");
 }
 
 //
@@ -55,8 +72,19 @@ duppage(envid_t envid, unsigned pn)
 	int r;
 
 	// LAB 4: Your code here.
-	panic("duppage not implemented");
-	return 0;
+	void *addr = (void*) (pn*PGSIZE);
+  if ((uvpt[pn] & PTE_W) || (uvpt[pn] & PTE_COW))
+  {
+      if (sys_page_map(0, addr, envid, addr, PTE_COW|PTE_U|PTE_P) < 0)
+            panic("in duppage: first sys_page_map wrong");
+        if (sys_page_map(0, addr, 0, addr, PTE_COW|PTE_U|PTE_P) < 0)
+            panic("in duppage: second sys_page_map wrong!");
+    }
+  else 
+    sys_page_map(0, addr, envid, addr, PTE_U|PTE_P);
+    
+  return 0;
+  //panic("duppage not implemented");
 }
 
 //
@@ -79,7 +107,39 @@ envid_t
 fork(void)
 {
 	// LAB 4: Your code here.
-	panic("fork not implemented");
+  set_pgfault_handler(pgfault);
+
+    envid_t envid;
+    uint32_t addr;
+    envid = sys_exofork();
+    if (envid == 0) 
+    {
+        thisenv = &envs[ENVX(sys_getenvid())];
+        return 0;
+    }
+    
+    if (envid < 0)
+        panic("sys_exofork: %e", envid);
+
+    for (addr = 0; addr < USTACKTOP; addr += PGSIZE)
+       
+    if ((uvpd[PDX(addr)] & PTE_P) && (uvpt[PGNUM(addr)] & PTE_P)&& (uvpt[PGNUM(addr)] & PTE_U))
+    {
+       duppage(envid, PGNUM(addr));
+    }
+
+    if (sys_page_alloc(envid, (void *)(UXSTACKTOP-PGSIZE), PTE_U|PTE_W|PTE_P) < 0)
+        panic("in fork: sys_page_alloc wrong!");
+ 
+    extern void _pgfault_upcall();
+ 
+    sys_env_set_pgfault_upcall(envid, _pgfault_upcall);
+
+    if (sys_env_set_status(envid, ENV_RUNNABLE) < 0)
+        panic("sys_env_set_status");
+
+    return envid;
+	//panic("fork not implemented");
 }
 
 // Challenge!
